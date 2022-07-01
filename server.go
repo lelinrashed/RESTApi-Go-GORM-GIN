54edbd32f65ba682535450e9e0912e4da900c486
lelinrashed <lelin.rashed784@gmail.com>
2022-07-01 21:53:40 +0600

    compleate user login, profile

--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/lelinrashed/RESTApi/config"
 	"github.com/lelinrashed/RESTApi/controller"
+	"github.com/lelinrashed/RESTApi/middleware"
 	"github.com/lelinrashed/RESTApi/repository"
 	"github.com/lelinrashed/RESTApi/service"
 	"gorm.io/gorm"
@@ -13,8 +14,10 @@ var (
 	db             *gorm.DB                  = config.SetupDatabaseConnection()
 	userRepository repository.UserRepository = repository.NewUserRepository(db)
 	jwtService     service.JWTService        = service.NewJWTService()
-	userService    service.AuthService       = service.NewAuthService(userRepository)
-	authController controller.AuthController = controller.NewAuthController(userService, jwtService)
+	userService    service.UserService       = service.NewUserService(userRepository)
+	authService    service.AuthService       = service.NewAuthService(userRepository)
+	authController controller.AuthController = controller.NewAuthController(authService, jwtService)
+	userController controller.UserController = controller.NewUserController(userService, jwtService)
 )
 
 func main() {
@@ -26,6 +29,13 @@ func main() {
 	{
 		authRoutes.POST("/login", authController.Login)
 		authRoutes.POST("/register", authController.Register)
+	}
+
+	// user route group
+	userRoutes := r.Group("api/user", middleware.AuthorizeJWT(jwtService))
+	{
+		userRoutes.GET("/profile", userController.Profile)
+		userRoutes.PUT("/profile", userController.Update)
 	}
 
 	// Run the server
